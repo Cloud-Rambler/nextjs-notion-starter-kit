8b5d7c2a9962c37078a4706d75aaa68b1161bf88
Cloud-Rambler <69468968+Cloud-Rambler@users.noreply.github.com>
2023-03-28 21:08:21 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Cloud-Rambler-s-blog-a88f9c8ddd534e52a719f291b8d61f88',
+  rootNotionPageId: 'a88f9c8ddd534e52a719f291b8d61f88',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
